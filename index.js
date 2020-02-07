86f68920fd0e49d8a4590ff010ab496938ee80d7
Paul Rosero <paul_rosero@yahoo.com>
2020-02-07 11:29:26 -0500

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-animal = ""
+const animal = ""
 
 function myAnimal() {
   return animal
@@ -9,7 +9,7 @@ function yourAnimal() {
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
   return animal
-}
+
 
 function add2(n) {
   return n + two
